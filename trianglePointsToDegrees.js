d21cb0017f841e1960e2b276c3828a2dc4511a19
Jason.Rowe <jason.rowe@gmail.com>
2013-02-10 17:38:17 -0600

    finish up initial triagle calcs

--- trianglePointsToDegrees.js
+++ trianglePointsToDegrees.js
@@ -1,3 +1,17 @@
+
+//Given a triangle with points (x1, y1, x2, y2, x3, y3),
+// finds angles A,B,C and edges a,b,c
+//
+//
+//     lower case a,b,c are the edges
+//     upper case A,B,C are angles
+//            (x1, y1)
+//               /\
+//            b / C\ a
+//             /    \
+//            /      \
+//    (x2,y2)/A______B\(x3, y3)
+//               c
 function TrianglePointsToDegrees(x1, y1, x2, y2, x3, y3){
 	
     //use distance formula to find lengths between (x1,y1) (x2,y2) and (x3,y3) named a,b,c
@@ -23,37 +37,47 @@ function TrianglePointsToDegrees(x1, y1, x2, y2, x3, y3){
 	log("create triangle edges");
 
 	var edgeA = {};
-
-	edgeA.points = [point1, point2];
-	edgeA.distance = lineDistance(point1, point2);
-	edgeA.oppositPoint = point3;
+	edgeA.points = [point3, point1];
+	edgeA.distance = lineDistance(point3, point1);
 
 	this.edgeA = function(){
 		return edgeA;
 	};
 
 	var edgeB = {};
-
-	edgeB.points = [point2, point3];
-	edgeB.distance = lineDistance(point2, point3);
-	edgeB.oppositPoint = point1;
+	edgeB.points = [point1, point2];
+	edgeB.distance = lineDistance(point1, point2);
 
 	this.edgeB = function(){
 		return edgeB;
 	};
 
 	var edgeC = {};
-
-	edgeC.points = [point3, point1];
-	edgeC.distance = lineDistance(point3, point1);
-	edgeC.oppositPoint = point2;
+	edgeC.points = [point2, point3];
+	edgeC.distance = lineDistance(point2, point3);
 
 	this.edgeC = function(){
 		return edgeC;
 	};
 
-	//Begin by using the cosine rule to find the largest angle. 
+    var sidea = edgeA.distance;
+    var sideb = edgeB.distance;
+    var sidec = edgeC.distance;
 
-	//The largest angle is always opposite to the largest side, so this is angle 
-		//
+	var anga = Math.acos((-sidea*sidea+sideb*sideb+sidec*sidec)/(2*sideb*sidec));
+	var angb = Math.acos((-sideb*sideb+sidea*sidea+sidec*sidec)/(2*sidea*sidec));
+	var angc = Math.acos((-sidec*sidec+sidea*sidea+sideb*sideb)/(2*sidea*sideb));
+
+	this.aDegree = function(){
+		return anga*180/Math.PI;
+	};
+
+	this.bDegree = function(){
+		return angb*180/Math.PI;
+	};
+
+	this.cDegree = function(){
+		return angc*180/Math.PI;
+	};
+
 }
--- trianglePointsToDegrees.tests.js
+++ trianglePointsToDegrees.tests.js
@@ -12,11 +12,26 @@ window.onload = function(){
 	//sanity test
 	assert(true, "the test suite is running.");
 
-	var triangle = new TrianglePointsToDegrees(1,3,-2,-2,3,3);
+	var triangle = new TrianglePointsToDegrees(1, 3, -2, -2, 3, -1);
 
 	assert(triangle.edgeA().points.length === 2, "points were created")
 
 	//check distance calculation
+	var distance = triangle.edgeB().distance;
+	assert(distance === 5.830951894845301, "lenth of edge b calculated")
+
+	var distance = triangle.edgeC().distance;
+	assert(distance === 5.0990195135927845, "lenth of edge c calculated")
+
 	var distance = triangle.edgeA().distance;
-	assert(distance === 5.830951894845301, "lenth was calculated")
+	assert(distance === 4.47213595499958, "lenth of edge a calculated")
+
+    var aDegree = triangle.aDegree();
+	assert(aDegree === 47.72631099390629, "Degree of angle A calculated")
+
+    var bDegree = triangle.bDegree();
+    assert(bDegree === 74.74488129694222, "Degree of angle B calculated")
+
+    var cDegree = triangle.cDegree();
+    assert(cDegree === 57.528807709151515, "Degree of angle C calculated")
 };
